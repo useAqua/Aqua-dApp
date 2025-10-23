c6759f9615d56e7b35effbbb641799635b23663a
James Osunkiyesi <jaydhales@gmail.com>
2025-10-23 05:29:15 +0100

    feat: Add Aqua Points Pool contract

---
+++ src/lib/contracts/aqua_poinst_pool.ts
@@ -0,0 +1,639 @@
+import { type Abi, getAddress } from "viem";
+import { env } from "~/env";
+
+const contract = {
+  address: getAddress(env.AQUA_POINTS_POOL_ADDRESS),
+  abi: [
+    {
+      inputs: [
+        { internalType: "address", name: "spender", type: "address" },
+        { internalType: "uint256", name: "allowance", type: "uint256" },
+        { internalType: "uint256", name: "needed", type: "uint256" },
+      ],
+      name: "ERC20InsufficientAllowance",
+      type: "error",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "sender", type: "address" },
+        { internalType: "uint256", name: "balance", type: "uint256" },
+        { internalType: "uint256", name: "needed", type: "uint256" },
+      ],
+      name: "ERC20InsufficientBalance",
+      type: "error",
+    },
+    {
+      inputs: [{ internalType: "address", name: "approver", type: "address" }],
+      name: "ERC20InvalidApprover",
+      type: "error",
+    },
+    {
+      inputs: [{ internalType: "address", name: "receiver", type: "address" }],
+      name: "ERC20InvalidReceiver",
+      type: "error",
+    },
+    {
+      inputs: [{ internalType: "address", name: "sender", type: "address" }],
+      name: "ERC20InvalidSender",
+      type: "error",
+    },
+    {
+      inputs: [{ internalType: "address", name: "spender", type: "address" }],
+      name: "ERC20InvalidSpender",
+      type: "error",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "user", type: "address" },
+        { internalType: "address", name: "token", type: "address" },
+      ],
+      name: "InsufficientStake",
+      type: "error",
+    },
+    { inputs: [], name: "InvalidInitialization", type: "error" },
+    { inputs: [], name: "NotInitializing", type: "error" },
+    {
+      inputs: [{ internalType: "address", name: "caller", type: "address" }],
+      name: "NotManager",
+      type: "error",
+    },
+    {
+      inputs: [{ internalType: "address", name: "owner", type: "address" }],
+      name: "OwnableInvalidOwner",
+      type: "error",
+    },
+    {
+      inputs: [{ internalType: "address", name: "account", type: "address" }],
+      name: "OwnableUnauthorizedAccount",
+      type: "error",
+    },
+    {
+      inputs: [{ internalType: "address", name: "token", type: "address" }],
+      name: "SafeERC20FailedOperation",
+      type: "error",
+    },
+    {
+      inputs: [{ internalType: "address", name: "token", type: "address" }],
+      name: "TokenNotActive",
+      type: "error",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "owner",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "spender",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "value",
+          type: "uint256",
+        },
+      ],
+      name: "Approval",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: false,
+          internalType: "address",
+          name: "newCalculator",
+          type: "address",
+        },
+      ],
+      name: "CalculatorUpdated",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: false,
+          internalType: "uint64",
+          name: "version",
+          type: "uint64",
+        },
+      ],
+      name: "Initialized",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "previousOwner",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "newOwner",
+          type: "address",
+        },
+      ],
+      name: "OwnershipTransferred",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "user",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "points",
+          type: "uint256",
+        },
+      ],
+      name: "PointsEarned",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "user",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "token",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "lpAmount",
+          type: "uint256",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "usdValue",
+          type: "uint256",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "receiptTokens",
+          type: "uint256",
+        },
+      ],
+      name: "Staked",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "token",
+          type: "address",
+        },
+      ],
+      name: "TokenAdded",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "from",
+          type: "address",
+        },
+        { indexed: true, internalType: "address", name: "to", type: "address" },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "value",
+          type: "uint256",
+        },
+      ],
+      name: "Transfer",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "user",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "token",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "lpAmount",
+          type: "uint256",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "receiptTokens",
+          type: "uint256",
+        },
+      ],
+      name: "Withdrawn",
+      type: "event",
+    },
+    {
+      inputs: [{ internalType: "address", name: "_token", type: "address" }],
+      name: "addToken",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "owner", type: "address" },
+        { internalType: "address", name: "spender", type: "address" },
+      ],
+      name: "allowance",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "spender", type: "address" },
+        { internalType: "uint256", name: "value", type: "uint256" },
+      ],
+      name: "approve",
+      outputs: [{ internalType: "bool", name: "", type: "bool" }],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "account", type: "address" }],
+      name: "balanceOf",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "currentSeason",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "decimals",
+      outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "_user", type: "address" }],
+      name: "earned",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "_token", type: "address" }],
+      name: "exit",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "_user", type: "address" },
+        { internalType: "address", name: "_token", type: "address" },
+      ],
+      name: "getUserCurrentUSDValue",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "_user", type: "address" },
+        { internalType: "address", name: "_token", type: "address" },
+      ],
+      name: "getUserDepositUSDValue",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "_user", type: "address" },
+        { internalType: "address", name: "_token", type: "address" },
+      ],
+      name: "getUserLPStake",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "_user", type: "address" },
+        { internalType: "address", name: "_token", type: "address" },
+      ],
+      name: "getUserReceiptTokens",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "_user", type: "address" },
+        { internalType: "address", name: "_token", type: "address" },
+      ],
+      name: "getUserStakeInfo",
+      outputs: [
+        { internalType: "uint256", name: "lpAmount", type: "uint256" },
+        { internalType: "uint256", name: "usdValueAtDeposit", type: "uint256" },
+        { internalType: "uint256", name: "currentUSDValue", type: "uint256" },
+        { internalType: "uint256", name: "receiptTokens", type: "uint256" },
+        { internalType: "int256", name: "pnl", type: "int256" },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "globalPointsDistributed",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "uint256", name: "_pointsPerSecond", type: "uint256" },
+        { internalType: "address", name: "_lpCalculator", type: "address" },
+      ],
+      name: "initialize",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "lpCalculator",
+      outputs: [
+        {
+          internalType: "contract ILPValueCalculator",
+          name: "",
+          type: "address",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "name",
+      outputs: [{ internalType: "string", name: "", type: "string" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "owner",
+      outputs: [{ internalType: "address", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "pointsInfo",
+      outputs: [
+        { internalType: "uint256", name: "pointsPerSecond", type: "uint256" },
+        { internalType: "uint256", name: "lastUpdateTime", type: "uint256" },
+        {
+          internalType: "uint256",
+          name: "pointsPerTokenStored",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "renounceOwnership",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "_token", type: "address" },
+        { internalType: "address", name: "_recipient", type: "address" },
+      ],
+      name: "rescueTokens",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "uint256", name: "", type: "uint256" },
+        { internalType: "address", name: "", type: "address" },
+      ],
+      name: "seasonPoints",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "_calculator", type: "address" },
+      ],
+      name: "setLPCalculator",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "uint256", name: "_pointsPerSecond", type: "uint256" },
+      ],
+      name: "setPointsPerSecond",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "_manager", type: "address" },
+        { internalType: "bool", name: "_whitelisted", type: "bool" },
+      ],
+      name: "setWhitelist",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "_token", type: "address" },
+        { internalType: "uint256", name: "_amount", type: "uint256" },
+      ],
+      name: "stake",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "startNewSeason",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "symbol",
+      outputs: [{ internalType: "string", name: "", type: "string" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      name: "tokenList",
+      outputs: [{ internalType: "address", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "", type: "address" }],
+      name: "tokens",
+      outputs: [
+        { internalType: "bool", name: "active", type: "bool" },
+        { internalType: "uint256", name: "totalLPStaked", type: "uint256" },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "totalSupply",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "", type: "address" }],
+      name: "totalUserPoints",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "_to", type: "address" },
+        { internalType: "uint256", name: "_value", type: "uint256" },
+      ],
+      name: "transfer",
+      outputs: [{ internalType: "bool", name: "", type: "bool" }],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "_from", type: "address" },
+        { internalType: "address", name: "_to", type: "address" },
+        { internalType: "uint256", name: "_value", type: "uint256" },
+      ],
+      name: "transferFrom",
+      outputs: [{ internalType: "bool", name: "", type: "bool" }],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "newOwner", type: "address" }],
+      name: "transferOwnership",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "updateMyPoints",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "_token", type: "address" },
+        { internalType: "bool", name: "_active", type: "bool" },
+      ],
+      name: "updateToken",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "", type: "address" },
+        { internalType: "address", name: "", type: "address" },
+      ],
+      name: "userStakes",
+      outputs: [
+        { internalType: "uint256", name: "lpAmount", type: "uint256" },
+        { internalType: "uint256", name: "receiptTokens", type: "uint256" },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "", type: "address" }],
+      name: "whitelisted",
+      outputs: [{ internalType: "bool", name: "", type: "bool" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "_token", type: "address" },
+        {
+          internalType: "uint256",
+          name: "_receiptTokenAmount",
+          type: "uint256",
+        },
+      ],
+      name: "withdraw",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+  ] as Abi,
+} as const satisfies { address: `0x${string}`; abi: Abi };
+
+export default contract;
