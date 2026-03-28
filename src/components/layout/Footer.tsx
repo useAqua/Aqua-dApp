0870a8b2ac8f6a48305d68129aa0b018bc63ad46
James Osunkiyesi <jaydhales@gmail.com>
2026-03-28 03:01:25 +0100

    revert: old footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { cn } from "~/lib/utils";
 
 const links = [
   // { label: "X", href: "https://x.com/useaqua_xyz" },
@@ -68,65 +69,65 @@ const links = [
 ];
 const Footer = () => {
   return (
-    <footer className="/*mt-8*/ relative overflow-x-clip pb-4">
-      <div className={"flex items-center gap-4 p-5"}>
-        {links.map((item) => (
-          <Link
-            href={item.href}
-            target={"_blank"}
-            key={item.label}
-            className="bg-card border-border/30 hover:bg-accent/50 text-foreground flex flex-1 cursor-pointer items-center justify-center gap-1 rounded-md border p-2 text-sm shadow-(--shadow-card) transition-colors active:scale-95"
-          >
-            {item.icon}
-            <span>{item.label}</span>
-          </Link>
-        ))}
+    <footer className="relative mt-8 overflow-x-clip pb-4">
+      {/*<div className={"flex items-center gap-4 p-5"}>*/}
+      {/*  {links.map((item) => (*/}
+      {/*    <Link*/}
+      {/*      href={item.href}*/}
+      {/*      target={"_blank"}*/}
+      {/*      key={item.label}*/}
+      {/*      className="bg-card border-border/30 hover:bg-accent/50 text-foreground flex flex-1 cursor-pointer items-center justify-center gap-1 rounded-md border p-2 text-sm shadow-(--shadow-card) transition-colors active:scale-95"*/}
+      {/*    >*/}
+      {/*      {item.icon}*/}
+      {/*      <span>{item.label}</span>*/}
+      {/*    </Link>*/}
+      {/*  ))}*/}
+      {/*</div>*/}
+      <div className="mx-auto mb-8">
+        <div className="flex items-center justify-center">
+          {[1, 2, 3].map((i) => (
+            <span
+              key={i}
+              className={cn(
+                "font-redaction text-[16.6666667vw] italic lg:text-[180px]",
+                i === 2
+                  ? "z-1 -mx-[3%]"
+                  : "text-footer-muted animate-glow animation-duration-[2500ms]",
+              )}
+            >
+              Aqua
+            </span>
+          ))}
+        </div>
       </div>
-      {/*<div className="mx-auto mb-8">*/}
-      {/*  <div className="flex items-center justify-center">*/}
-      {/*    {[1, 2, 3].map((i) => (*/}
-      {/*      <span*/}
-      {/*        key={i}*/}
-      {/*        className={cn(*/}
-      {/*          "font-redaction text-[16.6666667vw] italic lg:text-[200px]",*/}
-      {/*          i === 2*/}
-      {/*            ? "z-[1] -mx-[3%]"*/}
-      {/*            : "text-footer-muted animate-glow animation-duration-[2500ms]",*/}
-      {/*        )}*/}
-      {/*      >*/}
-      {/*        Aqua*/}
-      {/*      </span>*/}
-      {/*    ))}*/}
-      {/*  </div>*/}
-      {/*</div>*/}
-      {/*<div className="border-border border-t px-4 py-8">*/}
-      {/*  <div*/}
-      {/*    className={*/}
-      {/*      "container mx-auto flex items-center justify-between gap-4 max-md:flex-col"*/}
-      {/*    }*/}
-      {/*  >*/}
-      {/*    <p className="max-md:hidden">© 2025 Aqua</p>*/}
+      <div className="border-border border-t px-4 pt-4 pb-6">
+        <div
+          className={
+            "container mx-auto flex items-center justify-between gap-4 max-md:flex-col"
+          }
+        >
+          <p className="max-md:hidden">© 2025 Aqua</p>
 
-      {/*    <div className="flex flex-wrap justify-center gap-5 max-md:mb-4">*/}
-      {/*      {links.map((item) => (*/}
-      {/*        <div key={item.label} className="py-1">*/}
-      {/*          <Link*/}
-      {/*            href={item.href}*/}
-      {/*            className={cn(*/}
-      {/*              "text-foreground nav_link rounded-md px-0.5 py-1 underline transition-colors",*/}
-      {/*            )}*/}
-      {/*            target={"_blank"}*/}
-      {/*          >*/}
-      {/*            {item.label}*/}
-      {/*          </Link>*/}
-      {/*        </div>*/}
-      {/*      ))}*/}
-      {/*    </div>*/}
+          <div className="flex flex-wrap justify-center gap-5 max-md:mb-4">
+            {links.map((item) => (
+              <div key={item.label} className="py-1">
+                <Link
+                  href={item.href}
+                  className={cn(
+                    "text-foreground nav_link rounded-md px-0.5 py-1 underline transition-colors",
+                  )}
+                  target={"_blank"}
+                >
+                  {item.label}
+                </Link>
+              </div>
+            ))}
+          </div>
 
-      {/*    <p>Built with 💧 by Aqua Labs</p>*/}
-      {/*    <p className="md:hidden">© 2025 Aqua</p>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
+          <p>Built with 💧 by Aqua Labs</p>
+          <p className="md:hidden">© 2025 Aqua</p>
+        </div>
+      </div>
     </footer>
   );
 };
